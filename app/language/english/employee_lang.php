0b5869d0a24f9946b4b16b7a6e46250220cde754
s3do <s3do@s3do-Satellite-E105>
2012-10-13 13:43:24 +0300

    modify file language

--- app/language/english/employee_lang.php
+++ app/language/english/employee_lang.php
@@ -4,9 +4,40 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+$lang['employee_upload_Picture'] = "Setting Profile Page ";
 $lang['employee_profile'] = "Your Profile Page";
 $lang['employee_candidate'] = "List of candidaties Page";
 $lang['employee_candidate_Accepted'] = "The acceptance process was successful";
-// something confues me line 105 i will compelete it when faris told me the semantic
+$lang['employee_candidate_error'] = " There is a Problem";
+$lang['employee_candidate_reject_success'] = "The rejection process was successful";
+$lang['employee_candidate_reject_error'] = "There is a Problem";
+$lang['employee_candidate_Precaution_success'] = "Done , Precaution successfully";
+$lang['employee_candidate_Precaution_error'] = "There is a Problem";
+$lang['employee_accepted'] = "The list of accepted";
+$lang['employee_accepted_success'] = "The acceptance process was successful";
+$lang['employee_accepted_error'] = "There is a problem";
+$lang['employee_accepted_reject_success'] = "The rejection process was successful";
+$lang['employee_accepted_reject_error'] = "There is a Problem";
+$lang['employee_accepted_Precaution_success'] = "Done , Precaution successfully";
+$lang['employee_accepted_Precaution_error'] = "There is a Problem";
+$lang['employee_rejected'] = "The list of rejected";
+$lang['employee_rejected_success'] = "The rejectence process was successful";
+$lang['employee_rejected_error'] = "There is a problem";
+$lang['employee_rejected_reject_success'] = "The rejection process was successful";
+$lang['employee_rejected_reject_error'] = "There is a Problem";
+$lang['employee_rejected_Precaution_success'] = "Done , Precaution successfully";
+$lang['employee_rejected_Precaution_error'] = "There is a Problem";
+//
+$lang['employee_precaution'] = "The list of precaution";
+$lang['employee_precaution_success'] = "The precaution process was successful";
+$lang['employee_precaution_error'] = "There is a problem";
+$lang['employee_precaution_reject_success'] = "The precaution process was successful";
+$lang['employee_precaution_reject_error'] = "There is a Problem";
+$lang['employee_precaution_success'] = "Done , Precaution successfully";
+$lang['employee_precaution_error'] = "There is a Problem";
+$lang['employee_user'] = "Users List";
+$lang['employee_user_not_found'] = "Not found this user";
+$lang['employee_user_success'] = "The user has been deleted";
+$lang['employee_user_error'] = "There is a problem in the deletion process";
 
 ?>
