d0eacc661d064c6721d3bbb152232b2a356b5eaa
s3do <s3do@s3do-Satellite-E105>
2012-10-21 04:27:12 +0300

    modify file language

---
+++ app/language/english/upload_users.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//from views dir 
+
+$lang['upload_users_view_upload_user'] = "Upload users from csv file";
+$lang['upload_users_view_select_file'] = "Select File (less than 4 MB) :";
+$lang['upload_users_view_select_user'] = "Select Type Of users :";
+$lang['upload_users_view_second_step'] = "Second Step";
+
+
+
+?>
