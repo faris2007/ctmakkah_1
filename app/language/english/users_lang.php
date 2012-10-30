7087ac2e8f36a73ef1b34f6b602fa21498eab1a3
s3do <s3do@s3do-Satellite-E105>
2012-10-30 13:49:29 +0300

    modify file language and remove some line as a note to refrence to  the gloabal file

--- app/language/english/users_lang.php
+++ app/language/english/users_lang.php
@@ -10,15 +10,15 @@ $lang['user_view_search_by_id'] = "Search for any user By ID National";
 $lang['user_view_id'] = "National ID";
 $lang['user_view_bottom_search'] = "Search";
 $lang['user_view_list_user'] = "List Of Users";
-$lang['user_view_name'] = "Name";
-$lang['user_view_control'] = "Control";
-$lang['user_view_not_found'] = "Not Found";
-$lang['user_view_mobile'] = "Mobile";
+//$lang['user_view_name'] = "Name";
+//$lang['user_view_control'] = "Control";
+//$lang['user_view_not_found'] = "Not Found";
+//$lang['user_view_mobile'] = "Mobile";
 $lang['user_view_list_user_no_pic'] = "List Of Users Who don't have a Picture";
 $lang['user_view_not_found'] = "Not Found";
 $lang['user_view_position'] = "Position";
 $lang['user_view_download_csv'] = "Download AS CSV file";
-$lang['user_view_']
+
 
 
 ?>
